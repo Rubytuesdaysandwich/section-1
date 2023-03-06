9a035112818c6e3f8c3f5f3e1acdd68e5252cfa2
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-03-06 15:17:50 -0700

    update

--- section-2/basics-03-events-starting-code/basics-03-events-starting-code/app.js
+++ section-2/basics-03-events-starting-code/basics-03-events-starting-code/app.js
@@ -1,8 +1,29 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
+      name: "",
+      confirmedName: "",
     };
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num; //we do not need to return this value because we are using it as a listener on a button
+    },
+    subtract(num) {
+      //using the peramater we can allow any value in our html to be changed to add/sub any amount by simply changing the value in paranthesis
+      this.counter = this.counter - num;
+    },
+    setName(event, lastName) {
+      return (this.name = event.target.value + " " + lastName);
+    },
+    submitForm(event) {
+      // event.preventDefault(); this can be replace by the attribut modifier called prevent in vue js
+      alert("submitted");
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
   },
 });
 
