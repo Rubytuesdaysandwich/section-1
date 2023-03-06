8a46b6ad2502a0ac6c98d1f492480e5dde98cf76
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-03-06 16:05:08 -0700

    update

--- section-2/basics-assignment-2-problem/app.js
+++ section-2/basics-assignment-2-problem/app.js
@@ -1,11 +1,21 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      registerUser: "",
+      registerOnEnter: "",
+    };
   },
   methods: {
     alertUser() {
       // event.preventDefault(); this can be replace by the attribut modifier called prevent in vue js
       alert("This is an alert");
     },
+    userRegistration(event, lastName) {
+      return (this.registerUser = event.target.value + " " + lastName);
+    },
+    userRegistrationOnEnter(event, lastName) {
+      return (this.registerOnEnter = event.target.value + " " + lastName);
+    },
   },
 });
+app.mount("#assignment");
