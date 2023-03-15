c26e5ededbcf1244c9ca4871856997e10382e475
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-03-15 14:45:36 -0600

    section2 completed

--- section-2/basics-assignment-4-problem/basics-assignment-4-problem/app.js
+++ section-2/basics-assignment-4-problem/basics-assignment-4-problem/app.js
@@ -1,18 +1,24 @@
 const app = Vue.createApp({
   data() {
     return {
-      user1: false,
-      user2: false,
-      userInput: "",
+      InputIsActive: "", //accepting user input and then taking that user input and outputing it into the class attribute on stlye me! user1 and user2 works this way
+      toggleParagraphs: true,
+      inputBackgroundColor: "", //accept user input for color selection and send this to the style tag already in place to choose any color css accepts based on user input
     };
   },
+  computed: {
+    toggleClasses() {
+      return {
+        user1: this.InputIsActive === "user1",
+        user2: this.InputIsActive === "user2",
+        visible: this.toggleParagraphs,
+        hidden: !this.toggleParagraphs,
+      };
+    },
+  },
   methods: {
-    inputClass() {
-      if (this.userInput === "user1") {
-        this.user1 = true;
-      } else if (this.userInput === "user2") {
-        this.user2 = true;
-      }
+    toggleVisibility() {
+      this.toggleParagraphs = !this.toggleParagraphs;
     },
   },
 });
