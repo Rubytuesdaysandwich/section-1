647cafc3b68eeb8c90d6995a6d858f3e33d2d9d6
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-03-20 17:46:09 -0600

    update comopleted section 5 refs, hooks & cycle

---
+++ section-5/vue-life-lifecycle/app.js
@@ -0,0 +1,99 @@
+const app = Vue.createApp({
+  data() {
+    //first template
+    return {
+      currentUserInput: "",
+      message: "Vue is great!",
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+      //console.log(this.$refs.userText); //Vue provided property built in vue dollar sign is identifier of this
+    },
+  },
+  //!HOOKS▼▼▼▼▼▼▼▼
+  beforeCreate() {
+    console.log("beforeCreate()"); //nothing on screen at this point life cycle vue js
+  },
+  created() {
+    console.log("created()"); //nothing on screen at this point life cycle vue js
+  },
+  beforeMount() {
+    console.log("beforeMount()"); //nothing on screen at this point life cycle vue js
+  },
+  mounted() {
+    console.log("mounted()"); //appears on screen at this cycle
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate()");
+  },
+  updated() {
+    console.log("updated()");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount");
+  },
+  unmounted() {
+    console.log("unmounted");
+  },
+
+  //!Hooks you can use▲▲▲▲▲
+});
+
+app.mount("#app");
+
+setTimeout(function () {
+  app.unmount();
+}, 3000);
+///...VANILA JAVASCRIPT proxies
+
+let message = "hello";
+
+let longMessage = message + "world!";
+
+// console.log(longMessage);
+
+message = "Hello!!!!";
+
+// console.log(longMessage);
+
+//...
+//? we want message to change when longMessage changes.
+//*we want track one property and change another when that property changes
+const data = {
+  message: "Hello!",
+  longMessage: "Hello world!",
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === "message") {
+      target.longMessage = value + "world";
+    }
+    target.message = value;
+    // console.log(target);
+    // console.log(key);
+    // console.log(value);
+  },
+};
+const proxy = new Proxy(data, handler);
+proxy.message = " Hello!!!!";
+//! You can have multiple apps but they cannot grab variables from eachother
+
+//!==================
+
+const app2 = Vue.createApp({
+  template: `<p>{{favoriteMeal}}</p>`, //second template
+  data() {
+    return {
+      favoriteMeal: "pizza",
+    };
+  },
+});
+
+app2.mount("#app2");
