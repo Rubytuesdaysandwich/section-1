7d470cc420dfe9c776ae00b05cb9d9322d637b03
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-03-21 17:12:23 -0600

    update

---
+++ section-7.2/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>My Friends</h2>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "mauel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 654421 21",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
