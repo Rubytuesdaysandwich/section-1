7be14ad56506eb3f93e5924ef72875cb924e49d6
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-03-21 13:18:02 -0600

    update. learned about components

--- section-6/cmp-intro-01-starting-setup/app.js
+++ section-6/cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
-      displayContnet: false,
+      //   detailsAreVisible: false,
       friends: [
         {
           id: "manuel",
@@ -21,12 +21,49 @@ const app = Vue.createApp({
   watch: {},
   computed: {},
   methods: {
-    toggleDetails() {
-      this.displayContnet = !this.displayContnet;
-    },
+    //todo: moved to app.component: completed
+    // toggleDetails() {
+    //   this.detailsAreVisible = !this.detailsAreVisible;
+    // },
   },
 });
 
-//connecting the vuejs app to index.html page through the app ID.
+//should contain a - dash to avoid clashing with html elements
+//identifier = friend contact custom html tag
+// components are reusable pieces of html and logic and work stand alone. they can communicate with eachother encapsulate the code so you can work with smaller sections and have it be reusable. You can also choose to have these share
+app.component("friend-contact", {
+  template: `
+    <ul>
+        <li>
+          <h2>{{friend.name}}</h2>
+          <button @click="toggleDetails()">
+            {{detailsAreVisible ? 'Hide' : "Show"}}
+          </button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{friend.phone}}</li>
+            <li><strong>Email:</strong> {{friend.email}}</li>
+          </ul>
+        </li>
+      </ul>
+    `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+});
+//part of the parent app but secluded
 
 app.mount("#app");
+//connecting the vuejs app to index.html page through the app ID.
