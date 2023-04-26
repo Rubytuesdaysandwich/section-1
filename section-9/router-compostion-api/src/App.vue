54250cdf80a5f11afed03a3f217673bc8a2174cb
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-04-26 15:11:36 -0600

    update learn about global and local components

---
+++ section-9/router-compostion-api/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+//registering locally
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+
+export default {
+  components: {
+    //registering locally, key value pair
+    TheHeader,
+    BadgeList,
+    UserInfo,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ section-9/router-compostion-api/src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+
+import App from "./App.vue";
+import TheHeader from "./components/TheHeader.vue";
+import BaseBadge from "./components/BaseBadge.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+
+const app = createApp(App);
+//registering as global components. The downside to this is that the page loads all of them in this instance which makes your page more data intensive. Keep you pages lean by using multiple components.
+//we can load components locally we should do this for components like user-info and badge-list where they are being used only one time. This prevents them from being loaded without being needed on the page.
+app.component("the-header", TheHeader);
+app.component("base-badge", BaseBadge);
+// app.component("badge-list", BadgeList);
+// app.component("user-info", UserInfo);
+
+app.mount("#app");
