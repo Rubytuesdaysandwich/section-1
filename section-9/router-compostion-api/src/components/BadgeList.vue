4b3e267a41b5080568856da0c415eb959f9bfb0a
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-04-26 15:39:23 -0600

    UPDATE

--- section-9/router-compostion-api/src/components/BadgeList.vue
+++ section-9/router-compostion-api/src/components/BadgeList.vue
@@ -12,7 +12,15 @@
   </section>
 </template>
 
-<style>
+<style scoped>
+/* scoped keep your style within a page locally components with a key number vue generates for your pages*/
+section {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
 section h2 {
   margin: 0.5rem 0;
   color: #3a3a3a;
--- section-9/router-compostion-api/src/main.js
+++ section-9/router-compostion-api/src/main.js
@@ -3,7 +3,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import TheHeader from "./components/TheHeader.vue";
 import BaseBadge from "./components/BaseBadge.vue";
-// import BadgeList from "./components/BadgeList.vue";
+// import BadgeList from "./components/BadgeList.vue";//MOVED TO LOCALLY LOADED COMPONENTS
 // import UserInfo from "./components/UserInfo.vue";
 
 const app = createApp(App);
