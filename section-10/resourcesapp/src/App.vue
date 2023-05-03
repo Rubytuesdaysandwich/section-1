011af1dbb5d283fa5d50a346d078b18b40cdca71
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-05-03 16:45:08 -0600

    update

---
+++ section-10/resourcesapp/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <learning-resources
+      v-for="res in storedResources"
+      :key="res.id"
+    ></learning-resources>
+  </ul>
+</template>
+
+<script>
+//import components
+import LearningResources from "./components/learning-resources/LearningResource.vue";
+
+export default {
+  components: {
+    LearningResources,
+  },
+
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "official Guide",
+          description: "The official Vue.js ddocumentation.",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to google...",
+          link: "https://google.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ section-10/resourcesapp/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3></h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p></p>
+    <nav><a href="">view Resource</a></nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: [],
+};
+</script>
