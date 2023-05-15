3023ea6cf95ac7dbb6bfb2db21ddd787d14ccc8f
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-05-15 12:03:09 -0600

    sending to firebase fetch https

--- section-12/addingbackend/src/App.vue
+++ section-12/addingbackend/src/App.vue
@@ -1,33 +1,39 @@
 <template>
-  <learning-survey @survey-submit="storeSurvey"></learning-survey>
-  <user-experiences :results="savedSurveyResults"></user-experiences>
+  <learning-survey></learning-survey>
+  <!-- @survey-submit="storeSurvey" we no longer need -->
+  <user-experiences></user-experiences>
+  <!-- :results="savedSurveyResults" No longer need -->
 </template>
 
 <script>
-import LearningSurvey from './components/survey/LearningSurvey.vue';
-import UserExperiences from './components/survey/UserExperiences.vue';
+import LearningSurvey from "./components/survey/LearningSurvey.vue";
+import UserExperiences from "./components/survey/UserExperiences.vue";
 
 export default {
   components: {
     LearningSurvey,
     UserExperiences,
   },
-  data() {
-    return {
-      savedSurveyResults: [],
-    };
-  },
-  methods: {
-    storeSurvey(surveyData) {
-      const surveyResult = {
-        name: surveyData.userName,
-        rating: surveyData.rating,
-        id: new Date().toISOString(),
-      };
-      this.savedSurveyResults.push(surveyResult);
-      console.log(surveyResult);
-    },
-  },
+  //!since the data base firebase is being used we do not need this▼
+  //?However it is optional to keep local data stored as well and database
+  // data() {
+  //   return {
+  //     savedSurveyResults: [],
+  //   };
+  // },
+
+  // methods: {
+  //   storeSurvey(surveyData) {
+  //     const surveyResult = {
+  //       name: surveyData.userName,
+  //       rating: surveyData.rating,
+  //       id: new Date().toISOString(),
+  //     };
+  //     this.savedSurveyResults.push(surveyResult);
+  //     console.log(surveyResult);
+  //   },
+  // },
+  //!▲
 };
 </script>
 
--- section-12/addingbackend/src/components/survey/LearningSurvey.vue
+++ section-12/addingbackend/src/components/survey/LearningSurvey.vue
@@ -9,7 +9,13 @@
         </div>
         <h3>My learning experience was ...</h3>
         <div class="form-control">
-          <input type="radio" id="rating-poor" value="poor" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-poor"
+            value="poor"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-poor">Poor</label>
         </div>
         <div class="form-control">
@@ -23,12 +29,18 @@
           <label for="rating-average">Average</label>
         </div>
         <div class="form-control">
-          <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-great"
+            value="great"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-great">Great</label>
         </div>
-        <p
-          v-if="invalidInput"
-        >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="invalidInput">
+          One or more input fields are invalid. Please check your provided data.
+        </p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -41,30 +53,48 @@
 export default {
   data() {
     return {
-      enteredName: '',
+      enteredName: "",
       chosenRating: null,
       invalidInput: false,
     };
   },
-  emits: ['survey-submit'],
+  emits: ["survey-submit"],
   methods: {
     submitSurvey() {
-      if (this.enteredName === '' || !this.chosenRating) {
+      if (this.enteredName === "" || !this.chosenRating) {
         this.invalidInput = true;
         return;
       }
       this.invalidInput = false;
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
-      });
-
-      this.enteredName = '';
+      //! no longer needed this.$emit("survey-submit", {
+      //!   userName: this.enteredName,
+      //!   rating: this.chosenRating,
+      //! });
+      //built in browser support for grabing and sending data.
+      //sending https request to firebase to manage data ▼
+      fetch(
+        "https://learing-vue-js-backend-default-rtdb.firebaseio.com/surveys.json",
+        {
+          //get,post,delete,update, available options
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          //chosen from the emit method
+          body: JSON.stringify({
+            name: this.userName,
+            rating: this.chosenRating,
+          }),
+        }
+      );
+      //or you could use axios
+      this.enteredName = "";
       this.chosenRating = null;
     },
   },
 };
+//sending https request to firebase to manage data ▲
 </script>
 
 <style scoped>
@@ -72,7 +102,7 @@ export default {
   margin: 0.5rem 0;
 }
 
-input[type='text'] {
+input[type="text"] {
   display: block;
   width: 20rem;
   margin-top: 0.5rem;
