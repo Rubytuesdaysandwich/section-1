c5ee61743125cad7f244680ed327b78b6b5a3054
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-05-26 12:45:29 -0600

    update

--- section-15/vuex/src/App.vue
+++ section-15/vuex/src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <favorite-value></favorite-value>
+    <button @click="addOne">Add 10</button>
+    <change-counter></change-counter>
+  </base-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseContainer from "./components/BaseContainer.vue";
+import TheCounter from "./components/TheCounter.vue";
+import ChangeCounter from "./components/changeCounter.vue";
+import FavoriteValue from "./components/FavoriteValue.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavoriteValue,
+  },
+  // computed: {
+  //   counter() {
+  //     return this.$store.state.counter;
+  //   },
+  // },
+  methods: {
+    addOne() {
+      this.$store.commit("increase", { value: 10 }); //avoiding having replicating code using mutations
+      this.$store.commit({
+        type: "increase",
+        value: 10,
+      });
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
