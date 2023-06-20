65f82d411eb95b6d9e57bb2b56a90e8fbba34abf
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-06-20 13:03:26 -0600

    update

--- section-16/main-project-web-app/src/main.js
+++ section-16/main-project-web-app/src/main.js
@@ -4,10 +4,14 @@ import router from "./router.js";
 import store from "./store/index.js"; //importing store/ index.js to be used in the project
 import App from "./App.vue";
 import BaseCard from "./components/ui/BaseCard.vue";
+import BaseButton from "./components/ui/BaseButton.vue";
+import BaseBadge from "./components/ui/BaseBadge.vue";
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
 app.component("base-card", BaseCard);
+app.component("base-button", BaseButton);
+app.component("base-badge", BaseBadge);
 app.mount("#app"); //main app component of the app
