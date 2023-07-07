04ed80dc66b941f363d5bafb6a22bfc2e85e52c8
Rubytuesdaysandwich <88746844+Rubytuesdaysandwich@users.noreply.github.com>
2023-07-07 15:06:45 -0600

    update using vuex with comp api

---
+++ section-19.6/vuexcompositionapi/src/components/ControlCenter.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="inc">Increment</button>
+</template>
+
+<script>
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const store = useStore(); //you use this useStore within the vuex compositon api to access the vuex store instead of this.$store
+    function inc() {
+      store.dispatch("increment");
+    }
+
+    return { inc };
+  },
+};
+</script>
